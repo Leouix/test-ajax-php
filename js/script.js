8ebedd5a8171afbdfcd43d4870520672d2695e58
Leouix <ns24sola@gmail.com>
2022-07-03 05:53:09 +0300

    Modified descs

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 console.log( 'script jsesss')
 
-window.addEventListener('load', function (){
+window.addEventListener('load', function (){ // после загрузки страницы
 
-   $('#form-data').submit(function (e) {
-       e.preventDefault();
+   $('#form-data').submit(function (e) { // при помощи jQuery в момент отправки формы
+       e.preventDefault(); // останавливаем перезагрузку страницы
+       let dataForm = $(this).serialize(); // получаем данные с формы
+
+       // отправляем данные с формы по ajax на url
        $.ajax({
            url: 'includes/file.php',
-           data: $(this).serialize(),
+           data: dataForm,
            method: 'post',
            success(response) {
+               // в случае успеха передачи данных попадаем сюда
                console.log(response)
            }
        })
